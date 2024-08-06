dd1f002d5df60a7a95fc496a55769eb828f977af
Williams Hernández <hernandw@gmail.com>
2024-08-05 21:19:45 -0400

    :iterar los elementos del producto

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+
+
+const Card = ({name, images, description, price}) => {
+
+   /*  const { name, images, description, price } = zapato */
+
+  return (
+    <div className="col-md-6 col-lg-4 row align-items-center border p-3 rounded">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src={images}
+          alt="Imagen Zapaatos"
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fw-bold text-uppercase fs-4">{name}</h3>
+        <p className="text-black-50">
+          {description}
+        </p>
+        <p className="fw-black text-primary fs-3">${price}</p>
+       
+        <button type="button" className="btn btn-dark w-100">
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/data/data.js
@@ -0,0 +1,70 @@
+export const shoes = [
+    {
+      id: 1,
+      name: "Girls's Court High - 	Hi Ridge",
+      images: '/images/shoes1.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 34900,
+    },
+    {
+      id: 2,
+      name: "Girl's Court High - Shine Kicks",
+      images: '/images/shoes2.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 34900,
+    },
+    {
+      id: 3,
+      name: "Girls'Hi Ridge",
+      images: '/images/shoes3.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 33990,
+    },
+    {
+      id: 4,
+      name: "Girls' Hi Ridge - Superstardom",
+      images: '/images/shoes4.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 36990,
+    },
+    {
+      id: 5,
+      name: "Girls' Eden LX",
+      images: '/images/shoes5.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 32990,
+    },
+  
+    {
+      id: 6,
+      name: "Girls' Hi Ridge",
+      images: '/images/shoes6.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 30990,
+    },
+  
+    {
+      id: 7,
+      name: "Girls' Hi Ridge",
+      images: '/images/shoes7.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 30990,
+    },
+  
+    {
+      id: 8,
+      name: "Girls' Hi Ridge",
+      images: '/images/shoes8.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 30990,
+    },
+    {
+      id: 9,
+      name: "zapato nuevo",
+      images: '/images/shoes8.png',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 20000,
+    }
+  ];
+  
+

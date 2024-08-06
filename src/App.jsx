4737bfaaf764f66cf4e2c1a14c9eb8f48930d470
Williams Hernández <hernandw@gmail.com>
2024-08-05 20:03:19 -0400

    :tada: estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+
+
+
+const App = () => {
+  return (
+    <div className="container-xl mt-5">
+      <h2 className="text-center">Nuestra Coleccion</h2>
+
+      <div className="row mt-5 gap-5">
+
+      <div className="col-md-6 col-lg-4 row align-items-center border p-3 rounded">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src="./public/images/shoes1.png"
+          alt="Imagen Zapaatos"
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fw-bold text-uppercase fs-4">Zapatos</h3>
+        <p className="text-black-50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+        </p>
+        <p className="fw-black text-primary fs-3">$25.000</p>
+
+        <button type="button" className="btn btn-dark w-100">
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+    <div className="col-md-6 col-lg-4 row align-items-center border p-3 rounded">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src="./public/images/shoes1.png"
+          alt="Imagen Zapaatos"
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fw-bold text-uppercase fs-4">Zapatos</h3>
+        <p className="text-black-50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+        </p>
+        <p className="fw-black text-primary fs-3">$25.000</p>
+
+        <button type="button" className="btn btn-dark w-100">
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+    <div className="col-md-6 col-lg-4 row align-items-center border p-3 rounded">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src="./public/images/shoes1.png"
+          alt="Imagen Zapaatos"
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fw-bold text-uppercase fs-4">Zapatos</h3>
+        <p className="text-black-50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+        </p>
+        <p className="fw-black text-primary fs-3">$25.000</p>
+
+        <button type="button" className="btn btn-dark w-100">
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+    <div className="col-md-6 col-lg-4 row align-items-center border p-3 rounded">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src="./public/images/shoes1.png"
+          alt="Imagen Zapaatos"
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fw-bold text-uppercase fs-4">Zapatos</h3>
+        <p className="text-black-50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+        </p>
+        <p className="fw-black text-primary fs-3">$25.000</p>
+
+        <button type="button" className="btn btn-dark w-100">
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
